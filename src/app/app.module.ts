311f3d1fc36937b68415612733f4aaa66eb7fe0e
chrisdonell.tamennkouami <chrisdonell.tamennkouami@e-mothep.com>
2019-04-29 11:56:30 +0200

    ajout de la page identification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IndexPageComponent } from '../app/components/index-page/index-page.component';
+import { IdentificationComponent } from './components/identification/identification.component';
+import { LoginAdminComponent } from './components/login-admin/login-admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexPageComponent
+    IndexPageComponent,
+    IdentificationComponent,
+    LoginAdminComponent
   ],
   imports: [
     BrowserModule
