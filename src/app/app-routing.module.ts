73b878cdeb0a605d23da37184e9ebb1ab282713e
Chris Tamen <c.tamennkouami@epsi.fr>
2019-05-01 19:29:13 +0200

    Ajout d'admin-login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginAdminComponent } from './components/login-admin/login-admin.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IdentificationComponent } from './components/identification/identification.component';
@@ -5,9 +6,10 @@ import { IndexPageComponent } from './components/index-page/index-page.component
 
 
 const routes: Routes = [
-{ path: '', redirectTo: '/index-page', pathMatch: 'full' },
+{path: '', redirectTo: '/index-page', pathMatch: 'full' },
 {path: 'identification', component: IdentificationComponent },
 {path: 'index-page', component: IndexPageComponent},
+{path: 'login-admin', component: LoginAdminComponent}
   ];
 
 @NgModule({
